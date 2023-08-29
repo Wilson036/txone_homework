0bc3237898056ea7b0846f058d8e158c75d01be6
Kao Wilson <wilkao036@gmail.com>
2023-08-30 00:28:16 +0800

    完成menu test

--- src/components/MyMenu/MyMenu.test.tsx
+++ src/components/MyMenu/MyMenu.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import MyMenu from './index';
 import { TonicProvider } from '@tonic-ui/react';
@@ -45,5 +46,26 @@ describe('MyMenu', () => {
     expect(settingsItem).toBeInTheDocument();
     expect(homeIcon).toBeInTheDocument();
     expect(settingsIcon).toBeInTheDocument();
+  });
+
+  it('closes the menu when clicked', () => {
+    render(
+      <TonicProvider
+        colorMode={{
+          defaultValue: 'light',
+        }}
+      >
+        <MyMenu menuItems={mockMenuItems} />
+      </TonicProvider>
+    );
+
+    const menuButton = screen.getByLabelText('menu-button');
+    userEvent.click(menuButton);
+
+    const homeItem = screen.getByText('Home');
+    expect(homeItem).toBeInTheDocument();
+
+    userEvent.click(menuButton);
+    expect(homeItem).not.toBeVisible();
   });
 });
